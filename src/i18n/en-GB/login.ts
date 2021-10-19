923711d0d865482373367db35a5e8a2a01b07f27
MajoLkc <lukac.1majo@gmail.com>
2021-10-19 11:56:36 +0200

    Added basic translations for login page

---
+++ src/i18n/en-GB/login.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  LoginText: "Login into Crypto Tracker",
+  Email: "E-mail",
+  InsertEmail: 'Insert your email',
+  Password: "Password",
+  InsertPassword: 'Insert your password',
+  Login: "Login",
+  LoginErrorMsg: "The email or password entered is incorrect.",
+}
---
+++ src/i18n/sk-SK/login.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    LoginText: "Prihlás sa do Crypto Trackera",
+    Email: "E-mail",
+    InsertEmail: 'Zadaj svoj email',
+    Password: "Heslo",
+    InsertPassword: 'Zadaj svoje heslo',
+    Login: "Prihlásiť",
+    LoginErrorMsg: "Zadaný email alebo heslo nie je správne",
+  }
+  
