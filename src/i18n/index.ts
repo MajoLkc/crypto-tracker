5d64fd4ccff0c70a21600695e5f72bc4518b7c5b
MajoLkc <lukac.1majo@gmail.com>
2021-10-21 15:01:13 +0200

    Created index i18n file and uploaded to main index

---
+++ src/i18n/index.ts
@@ -0,0 +1,25 @@
+import i18NextInstance, { InitOptions } from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import en from './en-GB'
+import sk from './sk-SK'
+
+const debug = process.env.NODE_ENV === 'development'
+
+const config: InitOptions = {
+  resources: {
+    'en-GB': en,
+    'sk-SK': sk,
+  },
+  debug,
+  lng: 'en-GB',
+  fallbackLng: 'en-GB',
+  keySeparator: '.',
+  interpolation: {
+    escapeValue: false
+  },
+  saveMissing: debug,
+}
+
+i18NextInstance.use(initReactI18next).init(config)
+
+export const i18n = i18NextInstance
