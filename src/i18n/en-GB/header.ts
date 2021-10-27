5e0eb7d6a95ab670eb66252bf6f4193bf6be5d7d
MajoLkc <lukac.1majo@gmail.com>
2021-10-27 16:04:28 +0200

    Added translations for header

---
+++ src/i18n/en-GB/header.ts
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  SubText: "Track all your crypto transactions",
+  Portfolio: "Portfolio",
+  NewPurchase: "New exchange",
+  Profile: "Profile",
+  Logout: "Logout",
+}
---
+++ src/i18n/sk-SK/header.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  SubText: "Zaznamenaj všetky svoje krypto transakcie",
+  Portfolio: "Portfólio",
+  NewPurchase: "Nová zmena",
+  Profile: "Profil",
+  Logout: "Odhlásiť",
+}
